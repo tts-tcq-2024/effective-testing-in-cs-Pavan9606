7883e25cbb5e10c4db7613027ba1659e2e6e2164
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-08 19:51:52 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -9,15 +9,23 @@ namespace MisalignedSpace {
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);    //minorColors[i] instead of minorColors[j].
+                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[j]);
                 }
             }
-            return i * j;    // Returns 25, but we are asserting 24 to make the test fail
+            return i * j;
         }
+
         static void Main(string[] args) {
             int result = printColorMap();
-            Debug.Assert(result == 24); //expected result is 25
+            Debug.Assert(result == 25);
+          
+            Debug.Assert(CheckColorMapping());
             Console.WriteLine("All is well (maybe!)");
+        }
+
+        static bool CheckColorMapping() {
+
+            return false; // Simulate that mapping check fails
         }
     }
 }
