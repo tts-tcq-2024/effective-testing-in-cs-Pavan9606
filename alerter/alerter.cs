cf3ed08271a30bcc288df2c94543c34777f54122
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-08 19:51:09 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -3,28 +3,25 @@ using System;
 namespace AlerterSpace {
     class Alerter {
         static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
+         static Func<float, int> networkAlert = (celcius) => {
             Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
-            // Return 200 for ok
-            // Return 500 for not-ok
-            // stub always succeeds and returns 200
-            return 500;   //Return 200 for ok
+            return 200; 
+        }
+         public static void SetNetworkAlertFunction(Func<float, int> alertFunction) {
+            networkAlert = alertFunction;
         }
         static void alertInCelcius(float farenheit) {
             float celcius = (farenheit - 32) * 5 / 9;
-            int returnCode = networkAlertStub(celcius);
+            int returnCode = networkAlert(celcius);
             if (returnCode != 200) {
-                // non-ok response is not an error! Issues happen in life!
-                // let us keep a count of failures to report
-                // However, this code doesn't count failures!
-                // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;    
+                alertFailureCount += 1;
             }
         }
         static void Main(string[] args) {
-            alertInCelcius(400.5f);  //Should trigger a failure
-            alertInCelcius(303.6f);  //Should trigger a failure
-            Console.WriteLine("{0} alerts failed.", alertFailureCount);  // Should be 0 instead of 2
+            alertInCelcius(400.5f);
+            alertInCelcius(303.6f);
+            Console.WriteLine("{0} alerts failed.", alertFailureCount);
+            Debug.Assert(alertFailureCount == 2); // Test that failure count is as expected
             Console.WriteLine("All is well (maybe!)\n");
         }
     }
